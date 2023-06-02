b08a4b332cc43a4803a26664a728c57595e7063f
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-06-02 15:16:19 +0100

    update: added link instead of push

--- dapp-frontend/pages/index.tsx
+++ dapp-frontend/pages/index.tsx
@@ -1,14 +1,13 @@
 import Head from 'next/head';
-import { useRouter } from 'next/router';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Index() {
-  const { push } = useRouter();
 
-  const Button = ({ href, children }: any) => {
+  const Button = ({ href, children, target }: any) => {
     return (
-      <button onClick={() => push(href)} className='uppercase bg-gradient-to-br from-gray-800 to-teal-900 text-white shadow-md py-2 px-7 rounded-sm font-normal hover:shadow-lg duration-500'>
-        {children}
+      <button className='uppercase bg-gradient-to-br from-gray-800 to-teal-900 text-white shadow-md py-2 px-7 rounded-sm font-normal hover:shadow-lg duration-500'>
+        <Link href={href} target={target ? target : '_self'}><div>{children}</div></Link>
       </button>
     )
   }
@@ -25,7 +24,7 @@ export default function Index() {
             <p className='md:text-[2.2rem] text-['>High APR, Low Risk<br />Create pools for your project</p>
           </div>
           <div className='flex flex-row gap-x-3'>
-            <Button href="https://pancakeswap.finance/swap?outputCurrency=0x28B9aed756De31B6b362aA0f23211D13093EBb79">Buy<br /> Lunagens</Button>
+            <Button href="https://pancakeswap.finance/swap?outputCurrency=0x28B9aed756De31B6b362aA0f23211D13093EBb79" target='_blank'>Buy<br /> Lunagens</Button>
             <Button href="/staking">Create<br /> Pool</Button>
             <Button href="https://docs.google.com/forms/d/e/1FAIpQLSc9NK4x0_T0xcV9cp5ZoWPb5cVWUg09G_WGVTUcgxaC6HypWQ/viewform" target='_blank' >Request A<br /> Pool</Button>
           </div>
